ab63bffae536c6afd20cff5ebc4ee60657d203c2
fitnr <fitnr@fakeisthenewreal>
2016-03-11 14:59:48 -0500

    make data field positional

--- breaks/cli.py
+++ breaks/cli.py
@@ -16,10 +16,10 @@ from . import __version__, breaks, METHODS
 
 @click.command()
 @click.argument('infile', metavar='input', type=click.Path(exists=True))
-@click.argument('outfile', metavar='output', type=str)
 @click.option('-m', '--method', default='quantiles', help='Binning method (default: quantiles)',
               type=click.Choice([m.lower() for m in METHODS]))
-@click.option('-f', '--data-field', type=str, metavar='FIELD', required=True, help='data field to read')
+@click.argument('data-field', type=str, metavar='data-field')
+@click.argument('outfile', metavar='output', type=click.Path(writable=True, allow_dash=True))
 @click.option('-b', '--bin-field', type=str, metavar='FIELD', default='bin', help='name of new field')
 @click.option('-k', type=int, metavar='COUNT', default=5, help='Number of bins (default: 5)')
 @click.option('-B', '--bins', type=str, help='Comma-separated list of breaks (a series of upper-bounds)')
